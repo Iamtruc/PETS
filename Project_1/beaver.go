206604c9366943b0dc67eb33a70d76d2ba6e344a
Iamtruc <clement.lion@epfl.ch>
2020-03-04 08:36:53 +0100

    beaver_is_back

--- Project_1/beaver.go
+++ Project_1/beaver.go
@@ -1,1 +1,20 @@
 package main
+
+import(
+	"math/rand"
+)
+
+// We have n is the number of parties that are communicating, and l is the modulus of the nb
+
+func generatebeaver(nbparty,myrange int) (int, []int){
+	var a []int
+	var c int
+	for i := 0;i<nbparty;i++{
+		a = append(a, rand.Intn(myrange))
+	}
+
+	for _,i := range(a){
+		c += i
+	}
+	return c,a
+}
